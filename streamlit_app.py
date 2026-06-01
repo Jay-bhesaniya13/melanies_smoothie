eca9ef6241afb120c8add4db7b4d2b5e3ead6761
Jay Bhesaniya <143206706+Jay-bhesaniya13@users.noreply.github.com>
2026-06-01 15:03:18 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,10 @@
 # Import python packages
 import streamlit as st
-from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 
 # Get Snowflake session
-session = get_active_session()
+cnx=st.connection('snowflake')
+session = cnx.session
 
 # App title
 st.title(':cup_with_straw: Customize Your Smoothie!')
