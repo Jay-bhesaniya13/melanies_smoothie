fdf011287a09da94e6c378a858f1c44432e088f9
Jay Bhesaniya <143206706+Jay-bhesaniya13@users.noreply.github.com>
2026-06-01 15:53:10 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,7 +26,7 @@ my_dataframe = session.table("SMOOTHIES.PUBLIC.FRUIT_OPTIONS").select(col('FRUIT
 
 pd_df=my_dataframe.to_pandas()
 st.dataframe(pd_df)
-st.stop()
+ 
 
 
 ingredients_list=st.multiselect(
